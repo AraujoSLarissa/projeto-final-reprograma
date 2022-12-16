eea96b686bd0ace831d358ee28734d05415866b3
AraujoSLarissa <larissa.de.araujo@outlook.com>
2022-12-16 07:47:38 -0300

    adicionar crud finalizado

---
+++ src/controllers/atendimentosAcessiveisController.js
@@ -0,0 +1,173 @@
+const AtendimentosAcessiveisSchema = require("../models/AtendimentosAcessiveisSchema")
+
+const cadastrarAtendimento = async(req, res) => {
+    try {
+        const { 
+            nome, telefone, endereco, tipo_de_servico
+        } = req.body
+
+        if (!nome || nome.trim() === "") {
+            return res.status(400).send({ mensagem: "O nome é obrigatorio" })
+        } else if (isNaN(telefone) || telefone.length <= 0) {
+            return res.status(400).send({ mensagem: "O número de telefone é obrigatório" })
+        } else if (typeof(endereco.rua) !== "string" || endereco.rua.trim() === "") {
+            return res.status(400).send({ mensagem: "O campo rua é obrigatório" })
+        } else if (isNaN(endereco.numero)|| endereco.numero.length <= 0) {
+            return res.status(400).send({ mensagem: "O número do endereço é obrigatório" })
+        } else if (typeof(endereco.estado) !== 'string' || endereco.estado.trim() === "") {
+            return res.status(400).send({ mensagem: "O campo estado é obrigatório" })
+        } else if (typeof (endereco.cidade) !== 'string' || endereco.cidade.trim() === "") {
+            return res.status(400).send({ mensagem: "O campo cidade é obrigatório" })
+        } else if (typeof (endereco.bairro) !== 'string' || endereco.bairro.trim() === "") {
+            return res.status(400).send({ mensagem: "O campo bairro é obrigatório" })
+        } else if (!tipo_de_servico || tipo_de_servico.trim() === "") {
+            return res.status(400).send({ mensagem: "O tipo de serviço é obrigatorio" })
+        }
+        
+
+        let buscarBairro = await AtendimentosAcessiveisSchema.find({ endereco })
+
+        let checarBairro = buscarBairro.filter((atendimento) => atendimento.endereco.bairro === endereco.bairro)
+
+        let checarNomeAtendimento = checarBairro.find((atendimento) => atendimento.nome === nome)
+
+        if(checarNomeAtendimento){
+            return res.status(409).send({ mensagem: "Já existe atendimento com esse nome neste bairro"})
+        }
+
+        const atendimento = new AtendimentosAcessiveisSchema({
+            nome: nome,
+            telefone: telefone,
+            endereco: {
+                cep: endereco.cep,
+                rua: endereco.rua,
+                numero: endereco.numero,
+                complemento: endereco.complemento,
+                referencia: endereco.referencia,
+                estado: endereco.estado,
+                cidade: endereco.cidade,
+                bairro: endereco.bairro
+            },
+            tipo_de_servico: tipo_de_servico
+        })
+
+        atendimento.save()
+        res.status(201).json({
+            mensagem: "Serviço de atendimento psicológico cadastrado com sucesso!",
+            atendimento
+        })
+    } catch (error) {
+        res.status(400).send({ mensagem: error.message})
+    }
+}
+
+
+const buscarTodosAtendimentos = async(req, res) => {
+    const { nome } = req.query
+
+    let query = { }
+
+    if (nome) query.nome = new RegExp(nome, "i")
+
+    try {
+        const atendimentos = await AtendimentosAcessiveisSchema.find(query)
+        
+        if (atendimentos.length === 0) return res.status(200).send([])
+        
+        res.status(200).json({
+            mensagem: "Lista com todos os serviços de atendimento gratuito ou com preços acessíveis cadastrados",
+            atendimentos
+        })
+    } catch (error) {
+        res.status(500).send({ mensagem: error.message})
+    }
+}
+
+const buscarAtendimentoPorId = async(req, res) => {
+    try {
+        const { id } = req.params
+        const atendimento = await AtendimentosAcessiveisSchema.findById(id)
+        
+        if(atendimento == undefined || id == " ") {
+            return res.status(404).send({ mensagem: "Id não encontrado!"})
+        }
+
+        res.status(200).json(atendimento)
+    } catch (error) {
+        return res.status(500).send({ mensagem: error.message })
+    }
+}
+
+
+const atualizarAtendimentoPorId = async(req, res) => {
+    const { id } = req.params
+
+    try {
+        const { id } = req.params
+        const { nome, telefone, endereco, tipo_de_servico } = req.body
+
+
+        const atendimento = await AtendimentosAcessiveisSchema.findById(id)
+
+        if(!atendimento) {
+            return res.status(404).json({
+                message: "Serviço de atendimento psicológico não encontrado"
+            })
+        }
+
+        atendimento.nome = nome || atendimento.nome
+        atendimento.telefone = telefone || atendimento.telefone
+        atendimento.endereco.cep = endereco.cep || atendimento.endereco.cep
+        atendimento.endereco.rua = endereco.rua || atendimento.endereco.rua
+        atendimento.endereco.numero = endereco.numero || atendimento.endereco.numero
+        atendimento.endereco.complemento = endereco.complemento || atendimento.endereco.complemento
+        atendimento.endereco.referencia = endereco.referencia || atendimento.endereco.referencia
+        atendimento.endereco.estado = endereco.estado || atendimento.endereco.estado
+        atendimento.endereco.cidade = endereco.cidade || atendimento.endereco.cidade
+        atendimento.endereco.bairro = endereco.bairro || atendimento.endereco.bairro
+        atendimento.tipo_de_servico = tipo_de_servico || atendimento.tipo_de_servico
+        
+
+        const atendimentoAtualizado = await atendimento.save()
+
+        res.status(200).json({
+            mensagem: "Serviço atendimento psicológico atualizado!",
+            atendimentoAtualizado
+        })
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+}
+
+const deletarAtendimentoPorId = async(req, res) => {
+    try {
+        const { id } = req.params
+
+        const atendimento = await AtendimentosAcessiveisSchema.findById(id)
+
+        if(!atendimento) {
+            return res.status(404).json({
+                mensagem: "Atendimento não encontrado!"
+            })
+        }
+
+        await atendimento.delete()
+
+        res.status(200).json({
+            mensagem: "Atendimento psicológico deletado do sistema com sucesso!"
+        })
+    } catch (error) {
+        res.status(500).json({
+            mensagem: error.message
+        })
+    }
+}
+module.exports = {
+    cadastrarAtendimento,
+    buscarTodosAtendimentos,
+    buscarAtendimentoPorId,
+    atualizarAtendimentoPorId,
+    deletarAtendimentoPorId
+}
