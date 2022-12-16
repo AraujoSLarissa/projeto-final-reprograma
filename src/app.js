d73390a8a640285fa9e1fa707df24e0f7ef8b659
AraujoSLarissa <larissa.de.araujo@outlook.com>
2022-12-16 07:42:09 -0300

    adicionar modificação no app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const database = require("./database/mongoConnect")
+const atendimentosAcessiveis = require("./routes/atendimentosAcessiveisRoutes")
 const indexRouter = require("./routes/indexRoutes")
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 database.connect()
 
+app.use("/atendimentos-acessiveis", atendimentosAcessiveis)
 app.use(indexRouter)
 
 module.exports = app
